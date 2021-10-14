325a076f837a51f96717038de595eb54c83a6734
Mehmet Karagöz <eng.mkaragoz@gmail.com>
2021-10-14 16:24:47 +0300

    Initial commit.

---
+++ myDevice/mydevice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mydevice
+//
+//  Created by Mehmet Karagöz on 14.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
